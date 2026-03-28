e6e0ce81d160ba860ca50a7c38b39dfd758daa28
GiacomoVisin <killerboss1150@gmail.com>
2026-03-28 23:38:17 +0100

    snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,5 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorArray = posts.map((post)=> console.log(post.author))
