7efc30bcdf1bb2c7e051d0300c7e1085e698edd8
GiacomoVisin <killerboss1150@gmail.com>
2026-03-28 23:48:17 +0100

    snack 6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,24 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+
+const zucchinePlus15 = zucchine.filter((zucchina)=>{
+    if(zucchina.length >=  15 )
+        return true
+        
+})
+
+console.log(zucchinePlus15);
+
+
+
+
+
+const zucchineMinus15 = zucchine.filter((zucchina)=>{
+    if(zucchina.length <  15 )
+        return true
+        
+})
+
+console.log(zucchineMinus15);
